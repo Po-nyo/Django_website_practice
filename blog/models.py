e7d152215427c1668572b219de8ecfdb302e29b1
WooKyungjin <wkj1103@naver.com>
2020-04-29 05:22:08 +0900

    add Model - Category

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model) :
     title = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     contents = models.TextField()
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)

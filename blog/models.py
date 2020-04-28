119d29b1e8306fc407cb5f77df2ddaac32c0a9f8
WooKyungjin <wkj1103@naver.com>
2020-04-28 15:41:52 +0900

    model -> add image, media root, cardview update

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model) :
     title = models.CharField(max_length=30)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     contents = models.TextField()
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     date = models.DateTimeField()
 
     def __str__(self):

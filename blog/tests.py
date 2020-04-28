7e6e3770f478e03fa754deb5aa0e72dfe99d54f2
WooKyungjin <wkj1103@naver.com>
2020-04-29 01:44:31 +0900

    practice test.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, 'Blog')
+
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)

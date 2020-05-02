e21603bb307cb3365d3a500ab243ac0eb0c2b9f1
WooKyungjin <wkj1103@naver.com>
2020-05-03 04:16:07 +0900

    PostListByCategory

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post, Category
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post)
-admin.site.register(Category)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post, Category
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
-class PostList(ListView) :
+class PostList(ListView):
     model = Post
 
     def get_queryset(self):
@@ -17,13 +17,44 @@ class PostList(ListView) :
         return context
 
 
-class PostDetail(DetailView) :
+class PostDetail(DetailView):
     model = Post
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
         context['without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+class PostListByCategory(ListView):
+    model = Post
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            category = None
+        else:
+            category = Category.objects.get(slug=slug)
+
+        return Post.objects.filter(category=category).order_by('-date')
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListByCategory, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['without_category'] = Post.objects.filter(category=None).count()
+
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            category = None
+        else:
+            category = Category.objects.get(slug=slug)
+            context['title'] = 'Blog - {}'.format(category.name)
+
+        context['category'] = category
 
         return context
 

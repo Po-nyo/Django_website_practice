428f5d6489dbdfbc38c60f934c661373283ea6c4
WooKyungjin <wkj1103@naver.com>
2020-05-07 03:40:43 +0900

    PostListByTag

--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.PostListByCategory.as_view()),
+    path('tag/<str:slug>/', views.PostListByTag.as_view()),
 ]
